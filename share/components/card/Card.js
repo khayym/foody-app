69695ea1f41e6835946a6ffdc3161ed1ec774afd
GanievaSabina202 <61120111+GanievaSabina202@users.noreply.github.com>
2022-03-08 04:35:40 +0400

    HomePage (Feature) mobile & web design

--- share/components/card/Card.js
+++ share/components/card/Card.js
@@ -1,19 +1,19 @@
-// import Image from "next/image";
-// import { Wrapper, Wrap, ImageWrap, ContentWrap, Title, Desc } from './Card.syled'
+import Image from "next/image";
+import { Wrapper, Wrap, ImageWrap, ContentWrap, Title, Desc } from './Card.syled'
 const Card = () => {
     return (
         <>
-            {/* <Wrap>
+            <Wrap>
                 <Wrapper>
                     <ImageWrap>
-                        <Image src='/images/burger.svg' />
+                        <Image src='/images/burger.svg' width='239' height='239' />
                     </ImageWrap>
                     <ContentWrap>
                         <Title>Discount Boucher</Title>
                         <Desc>Lorem ipsum is placeholder  commonly used in the graphic </Desc>
                     </ContentWrap>
                 </Wrapper>
-            </Wrap> */}
+            </Wrap>
         </>
     );
 };
