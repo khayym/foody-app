35642578a1d10f85bb7412a620074ee38283c4c0
GanievaSabina202 <61120111+GanievaSabina202@users.noreply.github.com>
2022-03-25 01:34:23 +0400

    Order Page table add

--- features/user/orders/index.styled.js
+++ features/user/orders/index.styled.js
@@ -2,7 +2,6 @@ import styled from 'styled-components'
 import { ProfileComponent } from '../profil/profil.styled'
 
 export const OrderContainer = styled(ProfileComponent)`
-
 .orders_content{
     border:1px solid red;
 }
